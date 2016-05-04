82404057eb01e24e935b84c9da460fe01fbe4075
gitbahram <bahrambboy@gmail.com>
2016-05-04 23:36:56 +0430

    medicalProject

--- src/main/java/com/demisco/model/ObservBloodGlucose.java
+++ src/main/java/com/demisco/model/ObservBloodGlucose.java
@@ -1,9 +1,16 @@
 package com.demisco.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by bahram on 5/3/16.
  */
+@Entity
+@Table(name = "observ_blood_glucose")
 public class ObservBloodGlucose {
+    @Column(name = "region_name")
     private Long bloodGlucose;
 
     public Long getBloodGlucose() {
